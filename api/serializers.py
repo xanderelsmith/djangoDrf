d1f1df32fd1c3ee2949760aa624153b18e81db28
unknown <xanderelsmith@gmail.com>
2025-08-14 23:47:43 +0100

    completed api

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from books.models import Book
-
-class BooksSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Book
-        fields=('title','subtitle','author','isbn')
-        
--- api/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import BookApiView
-urlpatterns = [
- path('', BookApiView.as_view()),
-]
